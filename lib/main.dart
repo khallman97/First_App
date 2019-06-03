7858320da8012e881a6d5186a9c80a50802a922f
“yokhallman@my.yorku.cagit config --global user.email “yokhallman@my.yorku.cagit config --global user.name “Khallman97 <khallman@my.yorku.ca>
2019-06-03 19:04:33 -0400

    Updated list to cards and adjusted color

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      title: 'Startup Name Generator',     
-      theme: ThemeData(          // Add the 3 lines from here... 
-        primaryColor: Colors.grey[800],
-        scaffoldBackgroundColor: Colors.grey[400]
-      ),        
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+          // Add the 3 lines from here...
+          primaryColor: Colors.grey[800],
+          scaffoldBackgroundColor: Colors.grey[600]),
       home: RandomWords(),
     );
   }
 }
+
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
@@ -24,84 +24,107 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();   // Add this line.
+  final Set<WordPair> _saved = Set<WordPair>(); // Add this line.
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('Startup Name Generator'),
-      actions: <Widget>[      // Add 3 lines from here...
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          // Add 3 lines from here...
           IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],   
-    ),
-    body: _buildSuggestions(),
-  );
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
   }
-  Widget _buildSuggestions() {
-  return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: /*1*/ (context, i) {
-        if (i.isOdd) return Divider(); /*2*/
 
-        final index = i ~/ 2; /*3*/
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-        }
-        return _buildRow(_suggestions[index]);
-      });
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          // return _buildRow(_suggestions[index]);
+          bool alreadySaved = _saved.contains(_suggestions[index]);
+          return Card(
+              child: ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              // style:TextStyle(color: Colors.grey[300])
+            ),
+            trailing: Icon(
+              // Add the lines from here...
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red[600] : null,
+            ),
+            onTap: () {
+              // Add 9 lines from here...
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          ));
+        });
   }
+
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
-  return ListTile(
-    title: Text(
-      pair.asPascalCase,
-      style: _biggerFont,
-    ),
-    
-    trailing: Icon(   // Add the lines from here... 
-      alreadySaved ? Icons.favorite : Icons.favorite_border,
-      color: alreadySaved ? Colors.red : null,
-    ), 
-    onTap: () {      // Add 9 lines from here...
-      setState(() {
-        if (alreadySaved) {
-          _saved.remove(pair);
-        } else { 
-          _saved.add(pair); 
-        } 
-      });
-    },   
-  );
+    return ListTile(
+      title: Text(pair.asPascalCase, style: TextStyle(color: Colors.grey[300])),
+      trailing: Icon(
+        // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red[600] : null,
+      ),
+      onTap: () {
+        // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
   }
+
   void _pushSaved() {
-     Navigator.of(context).push(
-          MaterialPageRoute<void>(   // Add 20 lines from here...
-      builder: (BuildContext context) {
-        final Iterable<ListTile> tiles = _saved.map(
-          (WordPair pair) {
-            return ListTile(
-              title: Text(
-                pair.asPascalCase,
-                style: _biggerFont,
-              ),
-            );
-          },
-        );
-        final List<Widget> divided = ListTile
-          .divideTiles(
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // Add 20 lines from here...
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(pair.asPascalCase,
+                    style: TextStyle(color: Colors.grey[300])),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
             context: context,
             tiles: tiles,
-          )
-          .toList();
-          return Scaffold(         // Add 6 lines from here...
-          appBar: AppBar(
-            title: Text('Saved Suggestions'),
-          ),
-          body: ListView(children: divided),
-        );           
-      },
-    ), 
-  );
+          ).toList();
+          return Scaffold(
+            // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }
